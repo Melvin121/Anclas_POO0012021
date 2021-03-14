71156aa971d9aab1093cd30e0036cf427b3636ff
melvin121 <00016520@uca.edu.sv>
2021-03-13 20:22:22 -0600

    programa

---
+++ programa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Enter a value for a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} - {b} equals {AddNumbers(a, b)}")
+        }
+        public static int AddNumbers( int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
